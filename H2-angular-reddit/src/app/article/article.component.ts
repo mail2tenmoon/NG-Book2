9920cbeee4884d81abb986739586f678d97b2652
Logan Dupont <l.dupont@hotmail.be>
2018-01-19 23:04:24 +0100

    Add other projects

---
+++ H2-angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HostBinding } from '@angular/core';
+import { Article } from './article.model';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class')
+  cssClass = 'row';
+  @Input()
+  article: Article;
+
+  constructor() {
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
