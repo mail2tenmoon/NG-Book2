6390b3adebea6f1d8d82f39ed455546697badcf0
Logan Dupont <l.dupont@hotmail.be>
2019-06-12 23:19:37 +0200

    fix vulnerabilities advanced components project

--- NG-Book2-Advanced-Components/src/app/app.module.ts
+++ NG-Book2-Advanced-Components/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InlineStyleComponent } from './styling/inline-style/inline-style.component';
+
+
 
 
 @NgModule({
